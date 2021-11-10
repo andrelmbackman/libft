d584bd5119a6ee6ca54f2f2c7db5fe6428fc44e5
Andre Backman <abackman@student.hive.fi>
2021-11-10 13:27:17 +0200

    updated toupper test

--- eval_tests/ft_toupper_test.c
+++ eval_tests/ft_toupper_test.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:21:33 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/10 13:21:48 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/10 13:26:29 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main()
 	printf("toupper:    %s\n\n", str1);
 
 	printf("str: %s\n", str3);
-	str3[2] = toupper(str3[2]);
+	str3[2] = ft_toupper(str3[2]);
 	printf("ft_toupper: %s\n\n", str3);
 
 	printf("str: %s\n", str2);
@@ -33,7 +33,7 @@ int	main()
 	printf("toupper:    %s\n\n", str2);
 
 	printf("str: %s\n", str2);
-	str2[2] = toupper(str2[2]);
+	str2[2] = ft_toupper(str2[2]);
 	printf("ft_toupper: %s\n\n", str2);
 	return (0);
 }
