4264fb1653fedb5ecd48bfd19774ee54bf9d618b
Andre Backman <abackman@student.hive.fi>
2021-11-10 19:00:50 +0200

    created atoi and test

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 16:54:14 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/10 17:59:50 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/10 18:57:35 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,17 @@ int	ft_atoi(const char *nptr)
 	i = 0;
 	res = 0;
 	pos = 1;
-	while (nptr[i++] != '\0')
+	while ((nptr[i] >= '\b' && nptr[i] <= '\r') || nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
 	{
-		while (nptr[i] >= '\n' && nptr[i] <= '\v' && nptr[i] != '\0')
-		{
-			i++;
-		}
-		if (nptr[i] == '-')
-		{
-			pos = -1;
-			i++;
-		}
-		while (nptr[i] >= '0' && nptr[i] <= '9')
-		{
-			res = res * 10 + (nptr[i] - '0');
-			i++;
-		}
+		pos = -1 * (nptr[i] - '-' + 1);
+		i++;
+	}
+	while (nptr[i] >= '0' && nptr[i] <= '9')
+	{
+		res = res * 10 + (nptr[i] - '0');
+		i++;
 	}
 	return (res * pos);
 }
