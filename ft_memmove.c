5e3702d46d62bcc509a8c728c9d2afd5109b198c
Andre Backman <abackman@student.hive.fi>
2021-11-11 16:19:47 +0200

    changed memmove to cast to unsigned char

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,25 +6,23 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 12:37:33 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/04 14:08:34 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/11 16:15:55 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <stdlib.h>
 
 void	*ft_memmove(void *des, const void *src, size_t n)
 {
-	char			*str;
-	unsigned long	i;
-	char			*d;
-	char			*s;
+	unsigned char			*str;
+	unsigned long			i;
+	unsigned char			*d;
+	unsigned char			*s;
 
 	i = 0;
-	d = (char *)des;
-	s = (char *)src;
-	str = (char *)malloc(n * sizeof(char));
+	d = (unsigned char *)des;
+	s = (unsigned char *)src;
+	str = (unsigned char *)ft_memalloc(n * sizeof(unsigned char));
 	while (i < n)
 	{
 		str[i] = s[i];
