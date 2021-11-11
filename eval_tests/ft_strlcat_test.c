b0255c422ed343479845a4022a56206888d54455
Andre Backman <abackman@student.hive.fi>
2021-11-11 18:43:05 +0200

    updated ft_strlcat and test

--- eval_tests/ft_strlcat_test.c
+++ eval_tests/ft_strlcat_test.c
@@ -1,25 +1,68 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat
-	printf("after ft_strlcat:\nStr1        :+:      :+:    :+:   */
+/*   ft_strlcat_test.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:21:33 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/05 11:41:00 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/11 18:40:46 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-int	main()
+int main()
 {
-	char	str1[40] = "Hello World!";
-	char	str2[] = "... Goodbye World!";
-	printf("Str1: %s\nStr2: %s\n", str1, str2);
-	unsigned long i = ft_strlcat(str1, str2, 11);
-	printf("after ft_strlcat:\nStr1: %s\nThe size is %lu\n", str1, i);
-	return (0);
+    int tests_passed = 0;
+
+    char *s1 = malloc(sizeof(char) * 20);
+    char *s2 = malloc(sizeof(char) * 20);
+    char *src = " and this last";
+
+    s1[0] = '\0';
+    s2[0] = '\0';
+
+    size_t ret1 = ft_strlcat(s1, "this first", 20);
+    size_t ret2 = strlcat(s2, "this first", 20);
+
+    if (ret1 == ret2)
+        tests_passed++;
+    else
+        printf("ERROR: ret1 != ret2\n%zu\n%zu\n", ret1, ret2);
+
+    if (strcmp(s1, s2) == 0)
+        tests_passed++;
+    else
+        printf("ERROR: strcmp(s1, s2) != 0\n%s\n%s\n", s1, s2);
+
+    ret1 = ft_strlcat(s1, src, 20);
+    ret2 = strlcat(s2, src, 20);
+    if (ret1 == ret2)
+        tests_passed++;
+    else
+        printf("ERROR: ret1 != ret2\n%zu\n%zu\n", ret1, ret2);
+
+    if (strcmp(s1, s2) == 0)
+        tests_passed++;
+    else
+        printf("ERROR: strcmp(s1, s2) != 0\n%s\n%s\n", s1, s2);
+
+    ret1 = ft_strlcat(s1, src, 5);
+    ret2 = ft_strlcat(s2, src, 5);
+
+    if (ret1 == ret2)
+        tests_passed++;
+    else
+        printf("ERROR: ret1 != ret2\n%zu\n%zu\n", ret1, ret2);
+
+    free(s1);
+    free(s2);
+    if (tests_passed == 5)
+	{
+		printf("\tOK\n");
+        return (1);
+	}
+    return (0);
 }
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,25 +6,27 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 16:52:13 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/05 13:33:47 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/11 18:42:18 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-size_t	ft_strlcat(char *des, const char  *src, size_t size)
+size_t	ft_strlcat(char *des, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	j;
+	size_t	l;
 
 	i = ft_strlen(des);
 	j = 0;
-	while (size > 0)
+	l = i + ft_strlen(src);
+	while ((i + j) < dstsize && src[j] != '\0')
 	{
-		des[i++] = src[j++];
-		size--;
+		des[i + j] = src[j];
+		j++;
 	}
-	des[i] = '\0';
-	return (ft_strlen(des));
+	des[dstsize - 1] = '\0';
+	return (l);
 }
