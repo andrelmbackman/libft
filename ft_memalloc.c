308119ecb8e491b27924694b9b84241e04dfd3bc
Andre Backman <abackman@student.hive.fi>
2021-11-11 17:20:57 +0200

    updated ft_memalloc and test

---
+++ ft_memalloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 11:33:32 by abackman          #+#    #+#             */
+/*   Updated: 2021/11/11 17:19:13 by abackman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = (void *)malloc(size);
+	if (mem == NULL)
+		return (NULL);
+	else
+	{
+		ft_bzero(mem, size);
+		return (mem);
+	}
+}
