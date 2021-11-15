3fea3ccf8bf89521518d462a10c05e159ecfae8d
Andre Backman <abackman@student.hive.fi>
2021-11-15 17:39:00 +0200

    created ft_strequ and test

---
+++ eval_tests/ft_strequ_test.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 10:21:33 by abackman          #+#    #+#             */
+/*   Updated: 2021/11/15 17:37:38 by abackman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	main()
+{
+	int	tests_passed;
+	tests_passed = 0;
+	if (ft_strequ("ABC", "abc") == 0)
+		tests_passed++;
+	if (ft_strequ("Vote for Pedro", "Vote for Pedro") == 1)
+		tests_passed++;
+	if (ft_strequ("Vote for", "Vote for Pedro") == 0)
+		tests_passed++;
+	if (ft_strequ("", "Vote for Pedro") == 0)
+		tests_passed++;	
+	if (tests_passed == 4)
+		printf("Great success!\n\n");
+	else
+		printf("Error, check ft_strequ\n\n");
+	return (0);
+}
