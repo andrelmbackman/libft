19ef18ee7e0be27ec2bd6b57074f8b25ddb42e1f
Andre Backman <abackman@student.hive.fi>
2021-11-16 18:30:21 +0200

    updated strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 13:23:34 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/15 11:04:10 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/16 18:29:35 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,13 @@ char	*ft_strrchr(const char *s, int c)
 	i = 0;
 	j = ft_strlen(s);
 	p = NULL;
-	while (j >= i)
+	while (j > i && s[j] != c)
 	{	
-		if (s[j] == c)
-		{	
-			p = (char *)&s[j];
-			break ;
-		}
 		j--;
 	}
 	if (s[j] == c)
 	{
+		p = (char *)&s[j];
 		return (p);
 	}
 	else
