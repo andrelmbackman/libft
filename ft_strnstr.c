c173deebdde9914ff9591f47f4fb52cb5fbd940a
Andre Backman <abackman@student.hive.fi>
2021-11-16 18:02:06 +0200

    updated ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 13:35:56 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/15 11:04:03 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/16 18:01:17 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	p = NULL;
 	res = checkstrn(haystack, needle, 0, len);
+	if (len < 1)
+		return (NULL);
 	if (needle[0] == '\0')
 	{
 		return ((char *) haystack);
 	}
-	else if (res > 0)
+	else if (res >= 0)
 	{
 		p = (char *)&haystack[res];
 		return (p);
