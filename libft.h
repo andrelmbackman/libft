3cf5f30a84e1cf340e77c93b1bc45940bb685117
Andre Backman <abackman@student.hive.fi>
2021-11-17 16:51:11 +0200

    updated header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:16:24 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/16 10:57:14 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/17 16:46:37 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,8 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char	*ft_strsub(const char *s, unsigned int start, size_t len);
+char	**ft_strsplit(const char *s, char c);
 int		ft_strequ(const char *s1, const char *s2);
 int		ft_strnequ(const char *s1, const char *s2, size_t n);
 int		ft_strcmp(const char *s1, const char *s2);
@@ -58,5 +60,5 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
-#endif
 
+#endif
