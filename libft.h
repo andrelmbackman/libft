d5c3aef3639044717502f1c7a61c5f1ce1f5fc1a
Andre Backman <abackman@student.hive.fi>
2021-11-19 18:28:45 +0200

    updated header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 15:16:24 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/17 16:46:37 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/19 18:28:25 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 void	ft_putchar(char c);
 void	ft_putstr(char *s);
+void	ft_putendl(const char *s);
 void	ft_putnbr(int n);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *str, size_t len);
@@ -31,12 +32,13 @@ void	ft_strclr(char *s);
 void	ft_strdel(char **as);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+char	*ft_itoa(int n);
 char	*ft_strmap(const char *s, char (*f)(char));
-char	*ft_strmapi(const char *s, char (*f)(unsigned int, char)); 
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 size_t	ft_strlen(char const *str);
 char	*ft_strcat(char *des, const char *src);
 char	*ft_strncat(char *des, const char *src, size_t n);
-size_t	ft_strlcat(char *des, const char  *src, size_t size);
+size_t	ft_strlcat(char *des, const char *src, size_t size);
 char	*ft_strdup(char *str);
 char	*ft_strcpy(char *des, const char *src);
 char	*ft_strncpy(char *des, const char *src, size_t len);
@@ -46,6 +48,8 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strsub(const char *s, unsigned int start, size_t len);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strtrim(const char *s);
 char	**ft_strsplit(const char *s, char c);
 int		ft_strequ(const char *s1, const char *s2);
 int		ft_strnequ(const char *s1, const char *s2, size_t n);
