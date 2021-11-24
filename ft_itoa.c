04aac8d65114e1d5eea6a7010070186399754554
Andre Backman <abackman@student.hive.fi>
2021-11-24 13:24:39 +0200

    updated itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 14:19:59 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/19 16:46:42 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/24 13:23:47 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 static	int	ft_intlen(long n)
 {
-	if (n >= 1000000000)
-		return (10);
-	else if (n >= 100000000)
-		return (9);
-	else if (n >= 10000000)
-		return (8);
-	else if (n >= 1000000)
-		return (7);
-	else if (n >= 100000)
-		return (6);
-	else if (n >= 10000)
-		return (5);
-	else if (n >= 1000)
-		return (4);
-	else if (n >= 100)
-		return (3);
-	else if (n >= 10)
-		return (2);
-	else
+	int	length;
+
+	length = 0;
+	if (n == 0)
 		return (1);
+	if (n < 0)
+	{
+		n *= -1;
+		length++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		length++;
+	}
+	return (length);
 }
 
 char	*ft_itoa(int n)
@@ -43,17 +39,14 @@ char	*ft_itoa(int n)
 	long	num;
 
 	num = (long)n;
+	length = ft_intlen(num);
+	str = (char *)malloc((length + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
 	if (num < 0)
 	{
 		num *= -1;
-		length = ft_intlen(num) + 1;
-		str = (char *)ft_memalloc((length + 2) * sizeof(char));
 		str[0] = '-';
-	}
-	else
-	{
-		length = ft_intlen(num);
-		str = (char *)ft_memalloc((ft_intlen(num) + 1) * sizeof(char));
 	}
 	str[length--] = '\0';
 	while (length >= 0 && str[length] != '-')
