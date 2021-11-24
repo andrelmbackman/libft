9a250af32abed974e42c4092a25eadb82200d150
Andre Backman <abackman@student.hive.fi>
2021-11-24 18:05:36 +0200

    updated tests for strcmp and strequ

--- eval_tests/ft_strequ_test.c
+++ eval_tests/ft_strequ_test.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 10:21:33 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/15 17:39:32 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/16 11:51:36 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,22 @@ int	main()
 {
 	int	tests_passed;
 	tests_passed = 0;
-	if (ft_strequ("ABC", "abc") == 0)
+	if (ft_strnequ("ABC", "abc", 3) == 0)
 		tests_passed++;
-	if (ft_strequ("Vote for Pedro", "Vote for Pedro") == 1)
+	if (ft_strnequ("ABC", "ABc", 2) == 1)
 		tests_passed++;
-	if (ft_strequ("Vote for", "Vote for Pedro") == 0)
+	if (ft_strnequ("Vote for Pedro", "Vote for Pedro", 15) == 1)
 		tests_passed++;
-	if (ft_strequ("", "Vote for Pedro") == 0)
-		tests_passed++;	
-	if (tests_passed == 4)
+	if (ft_strnequ("Vote for", "Vote for Pedro", 15) == 0)
+		tests_passed++;
+	if (ft_strnequ("", "Vote for Pedro", 5) == 0)
+		tests_passed++;
+	if (ft_strnequ("Vote for P", "Vote for Pedro", 7) == 1)
+		tests_passed++;
+	if (tests_passed == 6)
+	{
 		printf("Great success!\n\n");
+	}
 	else
 		printf("Error, check ft_strequ\n\n");
 	return (0);
