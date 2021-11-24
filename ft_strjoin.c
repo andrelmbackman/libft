3d622332fb2899a65a86a31b7702edda8e2a32e5
Andre Backman <abackman@student.hive.fi>
2021-11-24 17:44:37 +0200

    added protection to strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 16:56:51 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/17 18:31:55 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/24 13:35:17 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@ char	*ft_strjoin(const char *s1, const char *s2)
 	size_t	j;
 	char	*str;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	i = (ft_strlen(s1) + ft_strlen(s2));
-	j = 0;
-	str = (char *)ft_memalloc((i + 1) * sizeof(char *));
-	i = 0;
+	str = (char *)malloc((i + 1) * sizeof(char *));
 	if (str == NULL)
 		return (NULL);
+	j = 0;
+	i = 0;
 	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
 		i++;
 	}
 	while (s2[j] != '\0')
-	{
 		str[i++] = s2[j++];
-	}
 	str[i] = '\0';
 	return (str);
 }
