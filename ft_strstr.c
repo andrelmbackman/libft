db682b58c4b8610d7e148a31041d8574a0d2e652
Andre Backman <abackman@student.hive.fi>
2021-11-24 13:22:49 +0200

    updated strstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,59 +6,42 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 16:57:43 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/23 17:55:35 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/24 13:18:40 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	int	checkstr(const char *h, const char *n, size_t i, size_t j);
-
-char	*ft_strstr(const char *haystack, const char *needle)
-{	
-	char	*p;
-	int		res;
-
-	p = NULL;
-	res = checkstr(haystack, needle, 0, 0);
-	if (res == 0)
-	{
-		return ((char *) haystack);
-	}
-	if (res > 0)
-	{
-		p = (char *)&haystack[res];
-		return (p);
-	}
-	else
-		return (NULL);
-}
-
-static	int	checkstr(const char *h, const char *n, size_t i, size_t j)
+static	int	ft_checkstr(const char *h, const char *n, size_t i, size_t j)
 {
 	if (n[j] == '\0')
-	{
 		return (0);
-	}
-	while (h[i] != '\0' && n[j] != '\0')
+	while (h[i] != '\0')
 	{
+		j = 0;
 		if (h[i] == n[j])
 		{
-			i++;
-			j++;
+			while (h[i + j] == n[j])
+			{
+				j++;
+				if (n[j] == '\0')
+					return (i);
+			}
 		}
-		else
-		{
-			j = 0;
-			i++;
-		}
+		i++;
 	}
-	if (n[j] == '\0')
-	{
-		return (i - j);
-	}
+	return (-1);
+}
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		res;
+
+	res = ft_checkstr(haystack, needle, 0, 0);
+	if (res == 0 || needle == NULL)
+		return ((char *)haystack);
+	if (res > 0)
+		return ((char *)&haystack[res]);
 	else
-	{
-		return (-1);
-	}
+		return (NULL);
 }
