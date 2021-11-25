4361e588189fe24a08c324344fb630527970f182
Andre Backman <abackman@student.hive.fi>
2021-11-25 14:34:55 +0200

    created lst functions

---
+++ ft_lstmap.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/25 12:38:29 by abackman          #+#    #+#             */
+/*   Updated: 2021/11/25 14:30:37 by abackman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*tmp;
+	t_list	*head;
+
+	if (!lst || !f)
+		return (NULL);
+	tmp = f(lst);
+	if (!tmp)
+		return (NULL);
+	head = tmp;
+	while (lst->next != NULL)
+	{
+		lst = lst->next;
+		tmp->next = f(lst);
+		if (!tmp)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp = tmp->next;
+	}
+	return (head);
+}
