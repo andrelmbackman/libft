79851c4a32e10fcf54a0a737367a0f07ec7fdb59
Andre Backman <abackman@student.hive.fi>
2021-11-26 18:06:28 +0200

    added protection to free entire 2d array in case of a malloc failure

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,35 +6,46 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 11:38:48 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/25 16:28:54 by abackman         ###   ########.fr       */
+/*   Updated: 2021/11/26 18:03:06 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static char	*add_nextword(const char *s, char c)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	while (s[i] != c && s[i] != '\0')
+		i++;
+	str = ft_strsub(s, 0, i);
+	return (str);
+}
+
 char	**ft_strsplit(const char *s, char c)
 {
 	char	**str;
-	size_t	i;
-	size_t	j;
+	size_t	words;	
 	size_t	k;
 
 	if (s == NULL)
 		return (NULL);
-	i = 0;
-	k = 0;
 	str = (char **)malloc((ft_countwords(s, c) + 1) * sizeof(char *));
 	if (str == NULL)
 		return (NULL);
-	while (k < ft_countwords(s, c))
+	words = ft_countwords(s, c);
+	k = 0;
+	while (k < words && s)
 	{
-		j = 0;
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		j = i;
-		while (s[i] != c && s[i] != '\0')
-			i++;
-		str[k] = ft_strsub(s, j, i - j);
+		while (*s == c && *s != '\0')
+			s++;
+		str[k] = add_nextword(s, c);
+		if (str[k] == NULL)
+			return ((char **)ft_free_arr(str, k - 1));
+		while (*s != c && *s != '\0')
+			s++;
 		k++;
 	}
 	str[k] = NULL;
