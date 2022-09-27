d510b228131dd0ba26f903bbb7d5e86199cf8f8a
Andre Backman <abackman@student.hive.fi>
2022-09-27 12:23:40 +0300

    removed extra curly brackets

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:06:35 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/10 15:52:16 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/27 12:23:19 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,7 @@
 int	ft_isalnum(int c)
 {
 	if ((ft_isalpha(c) == 1) || (ft_isdigit(c) == 1))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
