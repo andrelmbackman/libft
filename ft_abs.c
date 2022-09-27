fb3fe56733d7814e844be16cc263eecb20d06954
Andre Backman <abackman@student.hive.fi>
2022-09-27 12:20:29 +0300

    added comment

--- ft_abs.c
+++ ft_abs.c
@@ -6,11 +6,15 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 13:49:34 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/05 13:52:07 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/27 12:20:09 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Returns the absolute value of num, cast as a long long integer.
+*/
 
 long long	ft_abs(int num)
 {
