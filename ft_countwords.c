6356fed3dbfbeb10f1302509077c0d91385a88bf
Andre Backman <abackman@student.hive.fi>
2022-09-27 12:21:21 +0300

    added comment

--- ft_countwords.c
+++ ft_countwords.c
@@ -6,11 +6,15 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 16:27:59 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/25 16:29:21 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/27 12:21:14 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Returns the number of words in 's' with the delimiter 'c'.
+*/
 
 size_t	ft_countwords(const char *s, char c)
 {
