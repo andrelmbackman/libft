eeca4600f016fa9cdf43e815c3b8e8200f35a447
Andre Backman <abackman@student.hive.fi>
2022-09-29 18:52:33 +0300

    fixed ft_realloc double free

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:21:15 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/29 17:33:03 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/29 18:52:12 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ void	*ft_realloc(void *ptr, size_t size)
 	new = ft_memalloc(size);
 	if (new == NULL)
 		return (ptr);
-	ft_memcpy(new, ptr, size);
+	ft_memccpy(new, ptr, 0, size);
+	free(ptr);
 	return (new);
 }
