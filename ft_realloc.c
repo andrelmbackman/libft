b35de4e61676bc52650dd77f7d4fb693f9996aac
Andre Backman <abackman@student.hive.fi>
2022-10-03 17:00:40 +0300

    added variable to give content size

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,24 +6,26 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 17:21:15 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/29 18:52:12 by abackman         ###   ########.fr       */
+/*   Updated: 2022/10/03 16:06:41 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *ptr, size_t size)
+void	*ft_realloc(void *ptr, size_t ptr_size, size_t size)
 {
 	void	*new;
 
-	if (ptr == NULL)
+	if (ptr == NULL || ptr_size == 0)
 		return (ft_memalloc(size));
 	if (size < 1)
 		return (ptr);
 	new = ft_memalloc(size);
 	if (new == NULL)
 		return (ptr);
-	ft_memccpy(new, ptr, 0, size);
+	if (ptr_size > size)
+		ptr_size = size;
+	ft_memcpy(new, ptr, ptr_size);
 	free(ptr);
 	return (new);
 }
