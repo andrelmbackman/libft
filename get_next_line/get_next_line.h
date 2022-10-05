7094e7ca55d858da184f853cadb2193c4fd84fa4
Andre Backman <abackman@student.hive.fi>
2022-10-05 12:31:54 +0300

    increased buff_size in get_next_line

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/02 11:38:08 by abackman          #+#    #+#             */
-/*   Updated: 2021/12/16 17:56:52 by abackman         ###   ########.fr       */
+/*   Updated: 2022/10/05 12:31:17 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # include "../libft.h"
-# define BUFF_SIZE 42
+# define BUFF_SIZE 2048
 # define FD_SIZE 4096
 
 int	get_next_line(const int fd, char **line);
